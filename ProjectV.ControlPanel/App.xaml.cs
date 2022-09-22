da6a28db7108a91b6c8945310f22c8fb8d21725d
na1307 <na1307@outlook.kr>
2022-09-22 20:39:24 +0900

    제어판을 하나만 열 수 있도록 함

--- ProjectV.ControlPanel/App.xaml.cs
+++ ProjectV.ControlPanel/App.xaml.cs
@@ -9,6 +9,7 @@ global using System.Windows.Media;
 global using System.Windows.Media.Imaging;
 global using static ProjectV.BcdEdit;
 global using static ProjectV.GlobalConstants;
+using System.Threading;
 
 namespace ProjectV.ControlPanel;
 
@@ -16,6 +17,8 @@ namespace ProjectV.ControlPanel;
 /// App.xaml에 대한 상호 작용 논리
 /// </summary>
 public partial class App {
+    private static readonly Mutex mutex = new(true, "PVControl");
+
     internal static void SystemRestart() {
         using (Process shutdown = new() {
             StartInfo = new() {
@@ -32,6 +35,11 @@ public partial class App {
     }
 
     private void Application_Startup(object sender, StartupEventArgs e) {
+        if (!mutex.WaitOne(0)) {
+            ErrMsg("이미 다른 인스턴스가 실행 중입니다.");
+            Shutdown(1);
+        }
+
         if (PVConfig.Instance.Action != DoAction.DoNothing) {
             ErrMsg("대기 중인 작업(재구축 등)이 있는 것 같습니다. 지금은 다른 작업을 진행할 수 없습니다.\r\n\r\n먼저 재부팅하여 대기중인 작업을 처리하시기 바랍니다.");
             Shutdown(1);
