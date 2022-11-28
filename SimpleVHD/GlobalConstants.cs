90e3ba79e17a434197e387cd2eda992d57c9fc85
na1307 <na1307@outlook.kr>
2022-11-28 17:56:56 +0900

    라벨 수정

--- SimpleVHD/GlobalConstants.cs
+++ SimpleVHD/GlobalConstants.cs
@@ -7,7 +7,7 @@ public static class GlobalConstants {
     /// <summary>
     /// 빌드 번호
     /// </summary>
-    public const ulong BuildNumber = 170;
+    public const ulong BuildNumber = 171;
 
     /// <summary>
     /// 핵심 디렉토리 이름
