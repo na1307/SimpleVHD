9d4dc5fb17448404db83bf966f32dab41f63ea41
Jonghyo Lee <na1307@outlook.kr>
2024-05-17 22:13:44 +0900

    Checker

--- SimpleVhd.Library/Checker.cs
+++ SimpleVhd.Library/Checker.cs
@@ -2,17 +2,26 @@
 
 public static class Checker {
     public static void Check() {
-        var baseDirectory = AppContext.BaseDirectory;
-
-        if (new DriveInfo(baseDirectory).DriveType != DriveType.Fixed) {
-            throw new CheckException("SimpleVhd는 고정 드라이브에 위치해야 합니다.");
+        if (!Array.Exists(DriveInfo.GetDrives(), predicate)) {
+            throw new CheckException($"{SVDirName} 폴더를 찾을 수 없습니다.{Environment.NewLine}{Environment.NewLine}드라이브의 루트에 {SVDirName} 폴더가 있고 필요한 파일들이 모두 있는지 확인하세요.");
         }
 
-        var requires = new string[] { "Boot\\x64.wim", "Boot\\arm64.wim", "Boot\\boot.sdi" };
-        var requireNotFound = requires.Where(f => !File.Exists(Path.Combine(baseDirectory, "..", f)));
+        static bool predicate(DriveInfo drv) {
+            var isFixed = drv.DriveType == DriveType.Fixed;
+            var dirs = drv.RootDirectory.EnumerateDirectories().Where(dir => dir.Name == SVDirName);
+            var isSvDirExists = dirs.Any();
 
-        if (requireNotFound.Any()) {
-            throw new CheckException($"{requireNotFound.Select(f => Path.GetFileName(f)).ToJoinedString(", ")} 파일을 찾을 수 없습니다.");
+            return isFixed && isSvDirExists && isFilesExists(dirs.First(), ["Boot\\x64.wim", "Boot\\arm64.wim", "Boot\\boot.sdi"]);
+
+            static bool isFilesExists(DirectoryInfo directory, IEnumerable<string> fileNames) {
+                var value = true;
+
+                foreach (var fileName in fileNames) {
+                    value = value && File.Exists(Path.Combine(directory.FullName, fileName));
+                }
+
+                return value;
+            }
         }
     }
 }
---
+++ SimpleVhd.Library/Constants.cs
@@ -0,0 +1,6 @@
+﻿namespace SimpleVhd;
+
+public static class Constants {
+    public const ulong BuildNumber = 0;
+    public const string SVDirName = "SimpleVhd";
+}
