ec804f7d558af9dc63826abd3269473865623b40
Jonghyo Lee <na1307@outlook.kr>
2024-05-21 16:22:15 +0900

    WinUIEx

--- Publish-Core.cmd
+++ Publish-Core.cmd
@@ -1,7 +1,2 @@
-dotnet restore %1
-
-dotnet build %1 --no-restore -c:%2 -p=Platform=x64
-dotnet publish %1 --no-build -c:%2 -p=Platform=x64
-
-dotnet build %1 --no-restore -c:%2 -p=Platform=ARM64
-dotnet publish %1 --no-build -c:%2 -p=Platform=ARM64
+dotnet publish %1 -c %2 -p=Platform=x64
+dotnet publish %1 -c %2 -p=Platform=ARM64
--- SimpleVhd.ControlPanel/App.xaml.cs
+++ SimpleVhd.ControlPanel/App.xaml.cs
@@ -18,6 +18,7 @@ using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using WinRT.Interop;
+using WinUIEx;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -52,6 +53,10 @@ public sealed partial class App {
     /// <param name="args">Details about the launch request and process.</param>
     protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args) {
         m_window = new MainWindow();
+        m_window.SetWindowSize(750, 500);
+        m_window.SetIsResizable(false);
+        m_window.SetIsMaximizable(false);
+        m_window.CenterOnScreen();
         m_window.Activate();
     }
 
