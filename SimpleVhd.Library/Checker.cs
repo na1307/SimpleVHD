bf01bc34f586c6add3c0d975e7b530aa69ec5233
Jonghyo Lee <na1307@outlook.kr>
2024-05-22 20:32:50 +0900

    isOsInVhd

--- SimpleVhd.Library/Checker.cs
+++ SimpleVhd.Library/Checker.cs
@@ -1,4 +1,6 @@
-﻿namespace SimpleVhd;
+﻿using System.Management;
+
+namespace SimpleVhd;
 
 public static class Checker {
     public static void Check() {
@@ -6,6 +8,10 @@ public static class Checker {
 
         if (SVPath == string.Empty) {
             throw new CheckException($"{SVDirName} 폴더를 찾을 수 없습니다.{Environment.NewLine}{Environment.NewLine}드라이브의 루트에 {SVDirName} 폴더가 있고 필요한 파일들이 모두 있는지 확인하세요.");
+        }
+
+        if (!isOsInVhd()) {
+            throw new CheckException($"현재 가상 디스크로 부팅하지 않았습니다.{Environment.NewLine}{Environment.NewLine}가상 디스크로 부팅한 후 다시 시도하세요.");
         }
 
         static void checkSvPath() {
@@ -19,6 +25,14 @@ public static class Checker {
                     return;
                 }
             }
+        }
+
+        static bool isOsInVhd() {
+            var queryObj = new ManagementObjectSearcher(@"root\Microsoft\Windows\Storage", $"SELECT * FROM MSFT_PhysicalDisk WHERE DeviceID='{getSystemDiskNumber()}'").Get().Cast<ManagementBaseObject>().First();
+
+            return queryObj["PhysicalLocation"].ToString()![..22] is @"\Device\HarddiskVolume";
+
+            static int getSystemDiskNumber() => (int)(uint)new ManagementObjectSearcher("ASSOCIATORS OF {Win32_LogicalDisk.DeviceID='C:'} WHERE AssocClass=Win32_LogicalDiskToPartition").Get().Cast<ManagementBaseObject>().First()["DiskIndex"];
         }
     }
 }
